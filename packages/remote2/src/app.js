c2884ee1d87925c3f0bc553db5f1886f24d3b9f9
Niklas Mårdby <niklas.mardby@lynkco.com>
2023-12-15 12:15:55 +0100

    Added decrement to remote2

--- packages/remote2/src/app.js
+++ packages/remote2/src/app.js
@@ -7,20 +7,31 @@ const App = () => {
   const [count1] = useSlice("count1");
   const [count2] = useSlice("count2");
   const [localCount1, setLocalCount1] = useLocalSlice("count1");
-  const [localCount2, reduxDispatch, { increment }] = useLocalSlice("count2");
+  const [localCount2, reduxDispatch, { increment, decrement }] =
+    useLocalSlice("count2");
 
   return (
     <>
       <h1>Remote 2</h1>
-      <div>{count1}</div>
-      <div>{count2}</div>
+      <div>count1 {count1}</div>
+      <div>count2 {count2}</div>
       <div>
-        <button onClick={() => setLocalCount1((c) => c + 1)}>+</button>
-        {localCount1}
+        <button onClick={() => setLocalCount1((c) => c + 1)}>
+          localCount1 +
+        </button>
+        <button onClick={() => setLocalCount1((c) => c - 1)}>
+          localCount1 -
+        </button>
+        localCount1 {localCount1}
       </div>
       <div>
-        <button onClick={() => reduxDispatch(increment())}>+</button>
-        {localCount2}
+        <button onClick={() => reduxDispatch(increment())}>
+          localCount2 +
+        </button>
+        <button onClick={() => reduxDispatch(decrement())}>
+          localCount2 -
+        </button>
+        localCount2: {localCount2}
       </div>
     </>
   );
--- packages/remote2/src/slices.js
+++ packages/remote2/src/slices.js
@@ -4,6 +4,12 @@ import getHookAndProvider from "react-context-slices";
 export const { useSlice, Provider } = getHookAndProvider({
   slices: {
     count1: { initialArg: 0 },
-    count2: { initialState: 0, reducers: { increment: (state) => state + 1 } },
+    count2: {
+      initialState: 0,
+      reducers: {
+        increment: (state) => state + 1,
+        decrement: (state) => state - 1,
+      },
+    },
   },
 });
